4128808320cac81a600ee91a52df064d42a252b9
Matchu <matchu1993@gmail.com>
2011-03-30 08:51:07 -0400

    format timestamps

--- shop_warlock.rb
+++ shop_warlock.rb
@@ -9,6 +9,16 @@ end
 
 get '/items/:item_name' do |item_name|
   @item = Item.fetch(item_name)
-  "<img src='#{@item.image_url}' /><br/> Shop ID: #{@item.shop_id}<br/> #{@item.timestamps.inspect}"
+  haml :item
+end
+
+helpers do
+  # Formats integer representing the second of the day, e.g. 123, to the HH:MM
+  # format, e.g. 02:03
+  def format_timestamp(timestamp)
+    [timestamp / 60, timestamp % 60].map do |integer|
+      integer.to_s.rjust(2, '0')
+    end.join(':')
+  end
 end
 
