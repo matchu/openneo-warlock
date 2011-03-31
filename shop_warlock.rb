560721d2b43d6dc9a7e6d1f81d0c81b7d17b79b5
Matchu <matchu1993@gmail.com>
2011-03-31 14:09:00 -0400

    time expiry

--- shop_warlock.rb
+++ shop_warlock.rb
@@ -27,18 +27,48 @@ end
 
 ['/items/:name', '/items'].each do |path|
   get path do
-    @item = Item.fetch(params[:name]) if params[:name] && !params[:name].empty?
+    if params[:name] && !params[:name].empty?
+      @item = Item.fetch(params[:name])
+      @timestamps = @item.timestamps.map { |seconds| parse_timestamp(seconds) }
+    end
     haml :item, :layout => :item_layout
   end
+end
+
+def parse_timestamp(seconds)
+  [seconds / 60, seconds % 60]
 end
 
 helpers do
   # Formats integer representing the second of the day, e.g. 123, to the HH:MM
   # format, e.g. 02:03
   def format_timestamp(timestamp)
-    [timestamp / 60, timestamp % 60].map do |integer|
+    timestamp.map do |integer|
       integer.to_s.rjust(2, '0')
     end.join(':')
+  end
+
+  def timestamp_classname(timestamp)
+    hour = timestamp[0]
+    minute = timestamp[1]
+    "h#{hour} m#{minute}"
+  end
+
+  def timestamp_expiry_stylesheet
+    now = Time.now
+
+    expired_classes = []
+    0.upto(now.hour - 1) do |expired_hour|
+      expired_classes << ".h#{expired_hour}"
+    end
+
+    0.upto(now.min - 1) do |expired_min|
+      expired_classes << ".h#{now.hour}.m#{expired_min}"
+    end
+
+    expired_query = expired_classes.join ','
+
+    "<style type='text/css'>#{expired_query}{background:#aaa}</style>"
   end
 end
 
