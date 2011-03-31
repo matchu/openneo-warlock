1d818c2093cb0c24f534246c81f3fe56f760577b
Matchu <matchu1993@gmail.com>
2011-03-31 08:43:02 -0400

    set up compass

--- shop_warlock.rb
+++ shop_warlock.rb
@@ -1,12 +1,24 @@
+require 'compass'
 require 'sinatra'
+require 'haml'
 
 $:.unshift File.expand_path('../lib', __FILE__)
 require 'item'
 
+set :root, File.dirname(__FILE__)
 set :haml, :format => :html5
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
 
 error Neopets::Item::ItemNotFound do
   "Item not found"
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
 end
 
 get '/' do
